0f1b2db2ae99686bb56802ad234d9062cac19cdf
unknown <diegopagini777@gmail.com>
2020-10-16 11:41:16 -0300

    practicando un poco

--- src/index.js
+++ src/index.js
@@ -61,6 +61,8 @@ const myArrayOfPeople = [
   },
 ];
 
+/* -------------------- */
+
 // Mostrar los que tengan determinada edad:
 function showAge(edad, xArray) {
   let theAges = xArray.filter((people) => people.age === edad);
@@ -70,7 +72,7 @@ function showAge(edad, xArray) {
   mostrar = "";
   theAges.forEach((element) => {
     mostrar += `
-    <div>${element.name}</div>
+    <div class="div">${element.name}</div>
     `;
   });
   document.getElementById(
@@ -81,7 +83,128 @@ function showAge(edad, xArray) {
 showAge(20, myArrayOfPeople);
 showAge(22, myArrayOfPeople);
 
+/* -------------------- */
+
 // Mostrar los que tengan una profeion:
-function showProfession(profession) {
-  
+function showProfession(xProfession, xArray) {
+  let theProfessions = xArray.filter(
+    (people) => people.profession === xProfession
+  );
+  console.log(theProfessions);
+  mostrar = "";
+  theProfessions.forEach((element) => {
+    mostrar += `
+    <div class="div">${element.name}</div>
+    `;
+  });
+  document.getElementById(
+    "for"
+  ).innerHTML += `Las personas que tienen ${xProfession} como profesion son: ${mostrar}`;
 }
+showProfession("Engineer", myArrayOfPeople);
+showProfession("Football Player", myArrayOfPeople);
+
+/* -------------------- */
+
+// Mostrar los que vivan en algun pais:
+function showCountry(count, xArray) {
+  let theCountryes = xArray.filter((people) => people.country === count);
+  console.log(theCountryes);
+  mostrar = "";
+  theCountryes.forEach((element) => {
+    mostrar += `
+    <div class="div">${element.name}</div>
+    `;
+  });
+  document.getElementById(
+    "for"
+  ).innerHTML += `Las personas que viven en ${count} como son: ${mostrar}`;
+  // ).innerHTML += `Las personas que viven en ${count} como son: ${mostrar.name}`; Si en el div pongo solo el elemento de esta forma no puedo accesder y me muestra undefined
+}
+showCountry("España", myArrayOfPeople);
+showCountry("EEUU", myArrayOfPeople);
+
+/* -------------------- */
+
+// Mostrar todos los nombres del array
+function showNames(xArray) {
+  let nameOfPeople = xArray.map(() => {
+    return xArray.name;
+  });
+  mostrar = "";
+  nameOfPeople.forEach((element) => {
+    mostrar += `
+    <div class="div">${element}</div>
+    `;
+  });
+  document.getElementById(
+    "for"
+  ).innerHTML += `Todas las personas del array son: ${nameOfPeople}`;
+  // console.log(nameOfPeople); esto da undefined.
+}
+showNames(myArrayOfPeople);
+// No funca como espero.
+
+/* -------------------- */
+
+// Promedio de edades:
+let ages;
+function ageAverage(xArray) {
+  averageAges = 0;
+  xArray.forEach((people) => {
+    averageAges += people.age;
+  });
+
+  ages = averageAges / xArray.length;
+  //Hay una forma de hacerlo en una sola linea?
+  ages = ages.toFixed(2)
+
+  // agesx.toFixed(2) = averageAges / xArray.length; esto asi no anda
+
+
+  console.log("La edad promedio es de:", ages);
+
+  document.getElementById(
+    "for"
+  ).innerHTML += `<div class="div">La edad promedio de las personas es de : ${ages}</div>`;
+}
+
+ageAverage(myArrayOfPeople);
+
+/* -------------------- */
+
+// Mostrar todo de todo
+function showAll(){
+  myArrayOfPeople.forEach((people) => {
+    console.log(people.name);
+    console.log(people.age);
+    console.log(people.profession);
+    console.log(people.country);
+  })
+}
+showAll();
+// Bien, me muestra todo, pero.. y si ahora le hago un push?
+
+myArrayOfPeople.push({
+    name: "Dylan",
+    age: 75,
+    profession: "Retired",
+    country: "Russia",
+});
+
+// Y si ahora hago el showAll() ?
+showAll();
+// Mira que loco, funciona bien jaja.
+ageAverage(myArrayOfPeople);
+// Y tambien me modifia lo de las demas funciones.
+
+/* -------------------- */
+
+// La funcion mas reutilizable de las reutilizables. Mostrar los que sean "sorpresa" que otro "sorpresa"
+function surprise(sorpresa, sorpresa2, xArray) {
+  // let theSurprise = xArray.filter((people) => people.age sorpresa sorpresa2)
+};
+// No funciona, la idea era hacer 
+//surprise(<, 20, myArrayOfPeople)
+//surprise(>, 21, myArrayOfPeople)
+// Pero por lo visto no se puede usar un parametro como simbolo.
